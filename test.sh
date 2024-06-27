553260024494b075e0d5a30e96f659426c6f4cca
Peter Motzko <info@pmoscode.de>
2024-06-27 21:46:39 +0200

    feat: second draft

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-podman run \
-    --name embyserver \
-    --volume /Users/peter/Movies/Filme:/mnt/movies \
-    --publish 8096:8096 \
-    --restart on-failure \
-    emby/embyserver:4.8.3.0
